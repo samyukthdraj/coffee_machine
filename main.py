e82fe66a80cdcc330ecfa82b9c4f8f4f13a500bf
samyukthdraj <samyukthdharamarajan2002@gmail.com>
2023-11-11 17:23:50 +0400

    third commit

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 print("HI ALL USE 'WINDOWS + .' TO GET EMOJI KEYBOARD☕\n")
-from ingredients import MENU
-# TODO:Print the report, this gives us how much ingredients are in the machine
+from machine import MENU
+# TODO: 1. Print the report, this gives us how much ingredients are in the machine
 choice=input("What would you like to have? espresso\latte\cappucino: ").lower()
+# TODO: 2. As we enter an input, we should decrease the ingredients from the report
+# TODO: 2.1 then we should check if the ingredients are sufficient for the choice,
+# TODO: 2.2 then initially the money will be zero,
+# TODO: 2.3 we should input some money in nickels pennys quarters and dimes then we should return the money
+# TODO: 2.4 if excess money was added or say not enough money when there isnt enough money inputted.
