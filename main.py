171c07bac3d5c9f741c90477a7e8861b0ea1ea57
samyukthdraj <samyukthdharamarajan2002@gmail.com>
2023-11-11 17:11:57 +0400

    second commit

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-print("HI ALL\n")
+print("HI ALL USE 'WINDOWS + .' TO GET EMOJI KEYBOARD☕\n")
+from ingredients import MENU
+# TODO:Print the report, this gives us how much ingredients are in the machine
+choice=input("What would you like to have? espresso\latte\cappucino: ").lower()
